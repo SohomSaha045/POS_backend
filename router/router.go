7e2066f335519d66030891511fc347f6aab47aa8
Slutishboy <sohomsaha361@gmail.com>
2024-10-07 20:54:16 +0530

    Item-Controller-setup

--- router/router.go
+++ router/router.go
@@ -1,14 +1,12 @@
 package router
 
 import (
+	"github.com/SohomSaha045/POS_backend/controller"
 	"github.com/gorilla/mux"
 )
 
 func Router() *mux.Router {
 	router := mux.NewRouter()
-	// router.HandleFunc("/api",).Methods("GET")
-	
-	
-
+	router.HandleFunc("/item",controller.ItemController).Methods("GET")
 	return router
 }
